a5c39711c3a241c2880decc604231a2f8c8773ef
Ivey Topaz <iveytopaz@gmail.com>
2017-01-25 14:27:05 +0000

    Close tutorial after last box, add reducer for viewing previous

--- app/reducers/tutorialReducers.js
+++ app/reducers/tutorialReducers.js
@@ -10,11 +10,21 @@ const tutorial = (
 ) => {
   switch (action.type) {
     case 'OPEN_TUTORIAL':
-      return state.set('active', true);
+      return state.merge(Map({
+        active: true,
+        current: 1
+      }));
     case 'CLOSE_TUTORIAL':
       return state.set('active', false);
     case 'VIEW_NEXT':
-      return state.set('current', state.get('current') + 1);
+      return state.get('current') === state.get('totalPopUps') ?
+        state.merge(Map({
+          active: false,
+          current: 0
+        })) :
+        state.set('current', state.get('current') + 1);
+    case 'VIEW_PREVIOUS':
+      return state.set('current', state.get('current') - 1);
     case 'SET_LENGTH':
       return state.set('totalPopUps', action.totalPopUps);
     default:
