744a0316b9a160f0475727f31c8605f1e5b661f9
Ivey Topaz <iveytopaz@gmail.com>
2017-01-28 17:03:59 +0000

    Fix syntax errors in default code

--- app/reducers/userCode.js
+++ app/reducers/userCode.js
@@ -9,7 +9,7 @@ const themes = [
 ]
 
 const userCode = (state = {
-userCode: `function (tree, pause, highlight, augment) {
+userCode: `function Tree (tree, pause, highlight, augment) {
   class Node {
     constructor(value) {
       this.value = value;
@@ -18,16 +18,15 @@ userCode: `function (tree, pause, highlight, augment) {
     add(value) {
       this.children.push(new Node(value));
     }
-    traverse(callback) {
-      callback(this);
-      highlight(this);
+    traverse(cb) {
+      cb(this);
       for (let child of this.children) {
-        child.traverse(callback);
+        child.traverse(cb);
       }
     }
   }
-	let extendedTree = augment(tree, Node)
-	extendedTree.traverse(()=>true)
+	let extendedTree = augment(tree, Node);
+	extendedTree.traverse(highlight);
 
   let newTree = new Node(1)
   pause(newTree);
@@ -36,14 +35,54 @@ userCode: `function (tree, pause, highlight, augment) {
   newTree.add(3);
   return newTree;
 }`,
+altCode: `function BiTree (tree, pause, highlight, augment) {
+	class Node {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+    add(value) {
+      if (value > this.value) {
+        this.right ? this.right.add(value) : this.right = new Node(value);
+      } else if (value < this.value){
+        this.left ? this.left.add(value) : this.left = new Node(value);
+      }
+    }
+    traverse(cb) {
+      cb(this);
+      if (this.left) this.left.traverse(cb);
+      if (this.right) this.right.traverse(cb);
+    }
+  }
+  
+	let extendedTree = augment(tree, Node);
+	extendedTree.traverse(highlight);
+		
+	
+	let newTree = new Node(15);
+	for (var i = 0; i < 10; i++) {
+	  newTree.add(Math.floor(Math.random()*30));
+	  pause(newTree);
+	}
+	return newTree;
+}`,
 themeIndex: 0,
 theme: themes[0],
-themeOptions: themes.length
+themeOptions: themes.length,
+binary: false
 }, action) => {
   let newIndex;
   switch (action.type) {
     case 'UPDATE_CODE':
-      return {...state, userCode: action.newCode };
+      return { ...state, userCode: action.newCode };
+    case 'TOGGLE_TREE_TYPE':
+      return {
+        ...state,
+        userCode: state.altCode,
+        altCode: state.userCode,
+        binary: !state.binary
+      };
     case 'DARKEN_THEME':
       newIndex = Math.max(0, state.themeIndex - 1)
       return {
