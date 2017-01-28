0df66c44efafab0d45b426ecdb0961c2a5ff0fd6
Ivey Topaz <iveytopaz@gmail.com>
2017-01-28 11:31:20 +0000

    connect popover component to store

---
+++ app/containers/PopOver.js
@@ -0,0 +1,22 @@
+// @flow
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import PopOver from '../components/PopOver';
+import * as tutorialActions from '../actions/tutorialActions';
+
+function mapStateToProps(state) {
+  return {
+    tutorialActive: state.tutorial.get('active'),
+    currentlyDisplayedTutorial: state.tutorial.get('current'),
+    delay: state.async.delay,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      ...tutorialActions,
+    }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PopOver);
