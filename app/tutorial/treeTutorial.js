de594344274a8dd0dfd7aaeb1fb6e6e63637cab1
Ivey Topaz <iveytopaz@gmail.com>
2017-01-28 17:04:46 +0000

    Add info about binary trees

--- app/tutorial/treeTutorial.js
+++ app/tutorial/treeTutorial.js
@@ -36,74 +36,89 @@ const tutorialWindows = [{
   text: '"Children" must be an array, if you use "left" and "right", they should be Node objects or null.'
 }, {
   order: 8, 
-  xPos: '72%', 
-  yPos: '395px', 
-  text: 'Once you have defined your "Node" class, use "augment" to extend the passed in tree object with the functionality of the "Node" class.'
+  xPos: '70%', 
+  yPos: '540px', 
+  text: 'You can swap to starter code for a binary tree by clicking "Switch to Binary".'
 }, {
   order: 9, 
   xPos: '72%', 
   yPos: '395px', 
-  text: 'The "augment" function takes in an object and a class, and returns a new object. Each node in the new object will be of the "Node" class.'
+  text: 'Once you have defined your "Node" class, use "augment" to extend the passed in tree object with the functionality of the "Node" class.'
 }, {
   order: 10, 
   xPos: '72%', 
   yPos: '395px', 
-  text: 'Now you can call the methods defined on "Node" on the passed in tree object.'
+  text: 'The "augment" function takes in an object and a class, and returns a new object. Each node in the new object will be of the "Node" class.'
 }, {
   order: 11, 
+  xPos: '72%', 
+  yPos: '395px', 
+  text: 'Now you can call the methods defined on "Node" on the passed in tree object.'
+}, {
+  order: 12, 
   xPos: '72%', 
   yPos: '280px',
   text: 'When traverse gets called, it passes each node to highlight in order.'
 }, {
-  order: 12, 
+  order: 13, 
   xPos: '72%', 
   yPos: '280px', 
   text: 'Highlight will cause whatever node it is passed to turn red temporarily.'
 }, {
-  order: 13, 
+  order: 14, 
   xPos: '72%', 
   yPos: '280px',
   text: 'This allows you to see the order that a function traverses through your tree.'
 }, {
-  order: 14, 
+  order: 15, 
   xPos: '70%', 
   yPos: '445px', 
   text: 'The pause function takes in a tree, and puts it onto the visualizer temporarily.'
 }, {
-  order: 15, 
+  order: 16, 
   xPos: '70%', 
   yPos: '445px', 
   text: 'By calling pause and passing it "newTree" we can see the tree in-between adding nodes.'
 }, {
-  order: 16, 
+  order: 17, 
   xPos: '48%', 
   yPos: '70px', 
   text: 'You can control the speed of animation for highlighting and viewing tree states here.'
 }, {
-  order: 17, 
+  order: 18, 
   xPos: '48%', 
   yPos: '70px', 
   text: 'Warning: calling highlight on a large tree (30+ nodes) can run slowly.'
 }, {
-  order: 18, 
+  order: 19, 
   xPos: '10%', 
   yPos: '80px', 
   text: 'You can also move backwards and forwards through your visualizations here, up to 20 steps'
 }, {
-  order: 19, 
+  order: 20, 
   xPos: '68%', 
   yPos: '515px', 
   text: 'Finally, you should return a tree from this function. Whatever you return will end up in the visualizer.'
 }, {
-  order: 20, 
+  order: 21, 
   xPos: '68%', 
   yPos: '300px', 
   text: 'Edit the code to create your own tree...'
 }, {
-  order: 21, 
+  order: 22, 
   xPos: '83%', 
   yPos: '70px', 
   text: '...then click "Run" to see it in action.'
+}, {
+  order: 23, 
+  xPos: '65%', 
+  yPos: '450px', 
+  text: 'As an initial challenge, try to add more nodes to "newTree"...'
+}, {
+  order: 24, 
+  xPos: '68%', 
+  yPos: '300px', 
+  text: '...then refactor traverse to do a Breadth First traversal of the tree.'
 }]
 
 export default tutorialWindows;
