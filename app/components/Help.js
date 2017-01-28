e3d6796a23f8dd41e46912319e725f8174d9a23c
Ivey Topaz <iveytopaz@gmail.com>
2017-01-28 11:26:47 +0000

    Delete component

--- app/components/Help.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Help = ({ activate, styleClass }) => {
-  return (
-    <button className={styleClass} onClick={activate}>Help</button>
-  );
-};
-
-export default Help;
