3f6155f7e194e5c8b60b93673df2980722cc4f87
Ivey Topaz <iveytopaz@gmail.com>
2017-01-30 15:56:03 +0000

    Change header for example tree

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import AnimationSpeedSelector from './AnimationSpeedSelector';
 import UndoRedoCreator from '../containers/UndoRedo';
+import UserAction from './UserAction';
 import * as styles from '../styles/Home.scss';
 
 export default class Header extends Component {
@@ -30,7 +31,8 @@ export default class Header extends Component {
       darkenTheme,
       lightenTheme,
       themeIndex,
-      themeOptions
+      themeOptions,
+      headerType
     } = this.props;
     const UndoRedo = UndoRedoCreator(dataType);
     const backdropClass = tutorialActive ? styles.backdrop : styles.hidden;
@@ -40,23 +42,27 @@ export default class Header extends Component {
         <div className={backdropClass} onClick={closeTutorial} />
         <UndoRedo />
         <AnimationSpeedSelector delay={delay} changeDelay={this.changeDelay.bind(this)} />
-        <div>
-          <span className={styles.button_info}>Editor Theme</span>
-          <button
-            className={styles.darkerButton}
-            onClick={darkenTheme}
-            disabled={!themeIndex}
-          >Darker</button>
-          <button
-            className={styles.lighterButton}
-            onClick={lightenTheme}
-            disabled={themeIndex >= themeOptions - 1}
-          >Lighter</button>
-        </div>
-        <div className={styles.leftButtons} >
-          <button className={styles.runButton} onClick={runCode}>Run</button>
-          <button className={helpClass} onClick={openTutorial}>Help</button>
-        </div>
+        {headerType === 'code' ?
+          (<div>
+            <span className={styles.button_info}>Editor Theme</span>
+            <button
+              className={styles.darkerButton}
+              onClick={darkenTheme}
+              disabled={!themeIndex}
+            >Darker</button>
+            <button
+              className={styles.lighterButton}
+              onClick={lightenTheme}
+              disabled={themeIndex >= themeOptions - 1}
+            >Lighter</button>
+          </div>) : (<UserAction />)
+        }
+        {headerType === 'code' ?
+          <div className={styles.leftButtons} >
+            <button className={styles.runButton} onClick={runCode}>Run</button>
+            <button className={helpClass} onClick={openTutorial}>Help</button>
+          </div> : null
+        }
       </div>
     );
   }
