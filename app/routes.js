3691387bdf33e6c02899371de63497baea3ab807
Ivey Topaz <iveytopaz@gmail.com>
2017-01-30 15:57:29 +0000

    Add route for example structures, and home page

--- app/routes.js
+++ app/routes.js
@@ -2,11 +2,15 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
+import Menu from './components/Menu';
 import VerticalTree from './containers/VerticalTree';
+import AVLTree from './containers/AVLTree';
 
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={VerticalTree} />
+    <IndexRoute component={Menu} />
+    <Route path="usertree" component={VerticalTree} />
+    <Route path="treeExamples" component={AVLTree} />
   </Route>
 );
