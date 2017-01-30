866801e9a6ef09a58f67a7f20eb2329f37752c3c
Ivey Topaz <iveytopaz@gmail.com>
2017-01-30 16:00:14 +0000

    Add menu homepage

---
+++ app/components/Menu.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import * as styles from '../styles/Menu.scss';
+
+export default class Menu extends Component {
+  render() {
+    return (
+      <div className={styles.pageContainer}>
+        <div className={styles.menuContainer}>
+          <div className={styles.title}>Brancher</div>
+          <div className={styles.menuLinkContainer}>
+            <Link to='/usertree'><div className={styles.menuLink}>Write Code</div></Link>
+            <Link to='/treeExamples'><div className={styles.menuLink}>See Examples</div></Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
