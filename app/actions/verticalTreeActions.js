1d86fb05c2a3b347cd93bd4b97e2eafeb8177c31
Ivey Topaz <iveytopaz@gmail.com>
2017-02-01 16:38:27 +0000

    Use thunk to access delay state

--- app/actions/verticalTreeActions.js
+++ app/actions/verticalTreeActions.js
@@ -1,15 +1,11 @@
-const updateStructure = newState => {
-  return { type: 'UPDATE_VERT_STRUCTURE', newState };
+const updateStructure = newState => ({ type: 'UPDATE_VERT_STRUCTURE', newState });
+
+const highlightNode = nodeId => (dispatch, getState) => {
+  setTimeout(() => dispatch(unHighlightNode(nodeId)), getState().async.delay / 1.1);
+  dispatch({ type: 'HIGHLIGHT_NODE', nodeId });
 };
 
-const highlightNode = (delay, unHighlight, nodeId) => {
-  setTimeout(() => unHighlight(nodeId), delay / 1.1);
-  return { type: 'HIGHLIGHT_NODE', nodeId };
-};
-
-const unHighlightNode = nodeId => {
-  return { type: 'UNHIGHLIGHT_NODE', nodeId };
-};
+const unHighlightNode = nodeId => ({ type: 'UNHIGHLIGHT_NODE', nodeId });
 
 export default {
   updateStructure,
