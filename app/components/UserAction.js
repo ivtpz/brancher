4cae889707bd6888b5ddcc32c8cecaac69e20f5a
Ivey Topaz <iveytopaz@gmail.com>
2017-02-01 16:41:41 +0000

    fix spacing

--- app/components/UserAction.js
+++ app/components/UserAction.js
@@ -4,20 +4,27 @@ import * as styles from '../styles/Home.scss';
 export default class UserAction extends Component {
 
   handleAdd() {
-    this.props.buttonAction('add', parseInt(this.addInput.value));
+    this.props.buttonAction('add', parseInt(this.addInput.value, 10));
   }
 
   handleRemove() {
-    this.props.buttonAction('remove', parseInt(this.removeInput.value));
+    this.props.buttonAction('remove', parseInt(this.removeInput.value, 10));
   }
 
   render() {
-    const { buttonAction } = this.props;
     return (
       <div className={styles.userInput}>
-        <input ref={(input) => this.addInput = input} />
+        <input
+          ref={(input) => {
+            this.addInput = input;
+          }}
+        />
         <button onClick={this.handleAdd.bind(this)}>Add</button>
-        <input ref={(input) => this.removeInput = input} />
+        <input
+          ref={(input) => {
+            this.removeInput = input;
+          }}
+        />
         <button onClick={this.handleRemove.bind(this)}>Remove</button>
         <input /><button>Find</button>
       </div>
