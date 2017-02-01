e914811a3812a0eac45b6bd1bdc3996a2a60a399
Ivey Topaz <iveytopaz@gmail.com>
2017-02-01 08:44:38 +0000

    Add fonts for menu page

--- app/components/Menu.js
+++ app/components/Menu.js
@@ -7,10 +7,10 @@ export default class Menu extends Component {
     return (
       <div className={styles.pageContainer}>
         <div className={styles.menuContainer}>
-          <div className={styles.title}>Brancher</div>
-          <div className={styles.menuLinkContainer}>
-            <Link to='/usertree'><div className={styles.menuLink}>Write Code</div></Link>
-            <Link to='/treeExamples'><div className={styles.menuLink}>See Examples</div></Link>
+          <div className={styles.menuCircle}>
+            <div className={styles.title}>Brancher</div>
+             <Link to='/usertree'><div className={styles.menuLink}>Write Code</div></Link>
+             <Link to='/treeExamples'><div className={styles.menuLink}>See Examples</div></Link>
           </div>
         </div>
       </div>
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -38,7 +38,7 @@ export default validate({
       test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/,
       loader: "url-loader?limit=10000&mimetype=application/font-woff"
     }, { 
-      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      test: /\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
       loader: "file-loader"
     }]
   },
