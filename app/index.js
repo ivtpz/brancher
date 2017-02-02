01bdd9f4a184c5391c584ebbaa2bfbd320aef5df
Ivey Topaz <iveytopaz@gmail.com>
2017-02-02 16:09:01 +0000

    Reset dispayed tree when going to main menu

--- app/index.js
+++ app/index.js
@@ -5,9 +5,16 @@ import { Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import routes from './routes';
 import configureStore from './store/configureStore';
+import { resetTree } from './actions/verticalTreeActions';
 
 const store = configureStore();
 const history = syncHistoryWithStore(hashHistory, store);
+
+history.listen(location => {
+  if (location.pathname === '/') {
+    store.dispatch(resetTree());
+  }
+});
 
 render(
   <Provider store={store}>
