2d2893549d9a12cc921ef9ee62c5328a9903560c
Ivey Topaz <iveytopaz@gmail.com>
2017-02-02 16:07:14 +0000

    Keep user input in state

--- app/components/UserAction.js
+++ app/components/UserAction.js
@@ -2,31 +2,61 @@ import React, { Component } from 'react';
 import * as styles from '../styles/Home.scss';
 
 export default class UserAction extends Component {
-
-  handleAdd() {
-    this.props.buttonAction('add', parseInt(this.addInput.value, 10));
+  constructor(props) {
+    super(props);
+    this.state = {
+      add: '',
+      remove: '',
+      find: ''
+    };
+    this.components = {};
   }
 
-  handleRemove() {
-    this.props.buttonAction('remove', parseInt(this.removeInput.value, 10));
+  handleSubmit(prop) {
+    this.props.buttonAction(prop, parseInt(this.state[prop], 10));
+    let newState = this.state;
+    newState[prop] = '';
+    this.setState(newState);
+    this.components[prop].focus();
+  }
+
+  updateField(prop, e) {
+    let newState = this.state;
+    newState[prop] = e.target.value;
+    this.setState(newState);
   }
 
   render() {
+    const { asyncActive } = this.props;
     return (
       <div className={styles.userInput}>
         <input
-          ref={(input) => {
-            this.addInput = input;
-          }}
+          onChange={this.updateField.bind(this, 'add')}
+          value={this.state.add}
+          ref={(c) => { this.components.add = c; }}
         />
-        <button onClick={this.handleAdd.bind(this)}>Add</button>
+        <button
+          disabled={asyncActive}
+          onClick={this.handleSubmit.bind(this, 'add')}
+        >Add</button>
         <input
-          ref={(input) => {
-            this.removeInput = input;
-          }}
+          onChange={this.updateField.bind(this, 'remove')}
+          value={this.state.remove}
+          ref={(c) => { this.components.remove = c; }}
         />
-        <button onClick={this.handleRemove.bind(this)}>Remove</button>
-        <input /><button>Find</button>
+        <button
+          disabled={asyncActive}
+          onClick={this.handleSubmit.bind(this, 'remove')}
+        >Remove</button>
+        <input
+          onChange={this.updateField.bind(this, 'find')}
+          value={this.state.find}
+          ref={(c) => { this.components.find = c; }}
+        />
+        <button
+          disabled={asyncActive}
+          onClick={this.handleSubmit.bind(this, 'find')}
+        >Find</button>
       </div>
     );
   }
