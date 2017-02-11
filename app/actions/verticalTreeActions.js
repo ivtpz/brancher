31da3b8c85d762e2316d0fa589d7bfc615e191d4
Ivey Topaz <iveytopaz@gmail.com>
2017-02-11 15:39:12 +0000

    Find node by value when id is missing

--- app/actions/verticalTreeActions.js
+++ app/actions/verticalTreeActions.js
@@ -1,8 +1,11 @@
+import { findNodeIdByValue } from '../utils/vertTreeUtils';
+
 const updateStructure = newState => ({ type: 'UPDATE_VERT_STRUCTURE', newState });
 
-const highlightNode = nodeId => (dispatch, getState) => {
-  setTimeout(() => dispatch(unHighlightNode(nodeId)), getState().async.delay / 1.1);
-  dispatch({ type: 'HIGHLIGHT_NODE', nodeId });
+const highlightNode = (nodeId, nodeValue) => (dispatch, getState) => {
+  const Id = nodeId || findNodeIdByValue(nodeValue, getState().verticalTreeData.present.get(0));
+  setTimeout(() => dispatch(unHighlightNode(Id)), getState().async.delay / 1.1);
+  dispatch({ type: 'HIGHLIGHT_NODE', nodeId: Id });
 };
 
 const unHighlightNode = nodeId => ({ type: 'UNHIGHLIGHT_NODE', nodeId });
