19288160ef71d34f48a424eaea3055cc0cc14c93
Ivey Topaz <iveytopaz@gmail.com>
2017-02-11 15:38:21 +0000

    Allow multiple arguments for async actions

--- app/actions/asyncActions.js
+++ app/actions/asyncActions.js
@@ -6,11 +6,11 @@ const endAsync = () => ({ type: 'ASYNC_INACTIVE' });
 
 const startAsync = () => ({ type: 'ASYNC_ACTIVE' });
 
-const callAsync = (dispatchFn, newState) => (dispatch, getState) => {
+const callAsync = (dispatchFn, ...args) => (dispatch, getState) => {
   if (!asyncQueue.size) {
     asyncQueue.listenForEnd(dispatch.bind(null, endAsync()));
   }
-  asyncQueue.add(dispatchFn, getState().async.delay, newState);
+  asyncQueue.add(dispatchFn, getState().async.delay, ...args);
   dispatch(startAsync());
 };
 
