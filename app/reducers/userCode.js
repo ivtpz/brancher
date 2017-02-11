55f849017374ad04b16e5597fe99ae7440bc9506
Ivey Topaz <iveytopaz@gmail.com>
2017-02-11 15:39:32 +0000

    Move starter code to separate file

--- app/reducers/userCode.js
+++ app/reducers/userCode.js
@@ -1,4 +1,6 @@
-/* eslint-disable */
+import treeCode from '../starterCode/tree';
+import binaryTreeCode from '../starterCode/binaryTree';
+
 const themes = [
   'monokai',
   'tomorrow_night_eighties',
@@ -6,71 +8,15 @@ const themes = [
   'kuroir',
   'katzenmilch',
   'chrome'
-]
+];
 
 const userCode = (state = {
-userCode: `function Tree (tree, pause, highlight, augment) {
-  class Node {
-    constructor(value) {
-      this.value = value;
-      this.children = [];
-    }
-    add(value) {
-      this.children.push(new Node(value));
-    }
-    traverse(cb) {
-      cb(this);
-      for (let child of this.children) {
-        child.traverse(cb);
-      }
-    }
-  }
-	let extendedTree = augment(tree, Node);
-	extendedTree.traverse(highlight);
-
-  let newTree = new Node(1)
-  pause(newTree);
-  newTree.add(2);
-  pause(newTree);
-  newTree.add(3);
-  return newTree;
-}`,
-altCode: `function BiTree (tree, pause, highlight, augment) {
-	class Node {
-    constructor(value) {
-      this.value = value;
-      this.left = null;
-      this.right = null;
-    }
-    add(value) {
-      if (value > this.value) {
-        this.right ? this.right.add(value) : this.right = new Node(value);
-      } else if (value < this.value){
-        this.left ? this.left.add(value) : this.left = new Node(value);
-      }
-    }
-    traverse(cb) {
-      cb(this);
-      if (this.left) this.left.traverse(cb);
-      if (this.right) this.right.traverse(cb);
-    }
-  }
-  
-	let extendedTree = augment(tree, Node);
-	extendedTree.traverse(highlight);
-		
-	
-	let newTree = new Node(15);
-	for (var i = 0; i < 10; i++) {
-	  newTree.add(Math.floor(Math.random()*30));
-	  pause(newTree);
-	}
-	return newTree;
-}`,
-themeIndex: 0,
-theme: themes[0],
-themeOptions: themes.length,
-binary: false
+  userCode: treeCode,
+  altCode: binaryTreeCode,
+  themeIndex: 0,
+  theme: themes[0],
+  themeOptions: themes.length,
+  binary: false
 }, action) => {
   let newIndex;
   switch (action.type) {
@@ -84,16 +30,16 @@ binary: false
         binary: !state.binary
       };
     case 'DARKEN_THEME':
-      newIndex = Math.max(0, state.themeIndex - 1)
+      newIndex = Math.max(0, state.themeIndex - 1);
       return {
-        ...state, 
+        ...state,
         themeIndex: newIndex,
         theme: themes[newIndex]
       };
     case 'LIGHTEN_THEME':
-      newIndex = Math.min(state.themeOptions - 1, state.themeIndex + 1)
+      newIndex = Math.min(state.themeOptions - 1, state.themeIndex + 1);
       return {
-        ...state, 
+        ...state,
         themeIndex: newIndex,
         theme: themes[newIndex]
       };
