f3c3a98757e63d15f525367beeaa47d709729474
Ivey Topaz <iveytopaz@gmail.com>
2017-02-23 12:22:24 +0000

    Add linked list reducers

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 import { verticalTreeData } from './verticalTreeReducers';
+import { linkedListData } from './linkedListReducers';
 import userCode from './userCode';
 import async from './asyncReducers';
 import tutorial from './tutorialReducers';
@@ -8,6 +9,7 @@ import tutorial from './tutorialReducers';
 const rootReducer = combineReducers({
   routing,
   verticalTreeData,
+  linkedListData,
   userCode,
   async,
   tutorial
