1baee438c7ab206759c67618ff4717d1f9d15302
Ivey Topaz <iveytopaz@gmail.com>
2017-02-23 12:21:27 +0000

    Change route for options menu

--- app/components/Menu.js
+++ app/components/Menu.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router';
 import * as styles from './Menu.scss';
 
-export default class Menu extends Component {
-  render() {
-    return (
-      <div className={styles.pageContainer}>
-        <div className={styles.menuContainer}>
-          <div className={styles.menuCircle}>
-            <div className={styles.title}>Brancher</div>
-            <Link to='/usertree'><div className={styles.menuLink}>Write Code</div></Link>
-            <Link to='/treeExamples'><div className={styles.menuLink}>See Examples</div></Link>
-          </div>
+export default () => {
+  return (
+    <div className={styles.pageContainer}>
+      <div className={styles.menuContainer}>
+        <div className={styles.menuCircle}>
+          <div className={styles.title}>Brancher</div>
+          <Link to='/codeOptions'><div className={styles.menuLink}>Write Code</div></Link>
+          <Link to='/treeExamples'><div className={styles.menuLink}>See Examples</div></Link>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
