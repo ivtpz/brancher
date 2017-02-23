3aaebe26f06bd9a8cd804d3dd1c505ba3729903a
Ivey Topaz <iveytopaz@gmail.com>
2017-02-23 12:23:56 +0000

    Add routes for linked list in options menu

--- app/routes.js
+++ app/routes.js
@@ -2,15 +2,45 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
 import Menu from './components/Menu';
-import ExampleStructureOptions from './components/ExampleStructureOptions';
+import OptionsMenu from './components/OptionsMenu';
 import VerticalTree from './containers/VerticalTree';
 import ExampleTree from './containers/ExampleTree';
+import LinkedList from './containers/LinkedList';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Menu} />
-    <Route path="usertree" component={VerticalTree} />
-    <Route path="treeExamples" component={ExampleStructureOptions} />
+    <Route
+      path="codeOptions"
+      component={() => (
+        <OptionsMenu
+          links={[
+            {
+              path: '/codeTree',
+              text: 'Tree'
+            }, {
+              path: '/codeLinkedList',
+              text: 'Linked List'
+            }
+          ]}
+        />
+      )}
+    />
+    <Route
+      path="treeExamples"
+      component={() => (
+        <OptionsMenu
+          links={[
+            {
+              path: '/AVLTree',
+              text: 'AVLTree'
+            }
+          ]}
+        />
+      )}
+    />
+    <Route path="codeTree" component={VerticalTree} />
+    <Route path="codeLinkedList" component={LinkedList} />
     <Route path="AVLTree" component={() => (<ExampleTree dataStructure='AVLCreator' />)} />
   </Route>
 );
