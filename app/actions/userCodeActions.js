aee26ca3bd8da1be1f2582aba65364ae13f0416d
Dev <dev@adams-mbp.home>
2017-02-23 15:42:27 +0000

    Add more starter code options

--- app/actions/userCodeActions.js
+++ app/actions/userCodeActions.js
@@ -2,6 +2,18 @@ const updateCode = newCode => {
   return { type: 'UPDATE_CODE', newCode };
 };
 
+const updateTreeCode = newCode => {
+  return { type: 'UPDATE_TREE_CODE', newCode };
+};
+
+const updateBinaryCode = newCode => {
+  return { type: 'UPDATE_BINARY_CODE', newCode };
+};
+
+const updateLinkedListCode = newCode => {
+  return { type: 'UPDATE_LINKED_LIST_CODE', newCode };
+};
+
 const swapCode = () => {
   return { type: 'TOGGLE_TREE_TYPE' };
 };
@@ -16,6 +28,9 @@ const lighten = () => {
 
 export default {
   updateCode,
+  updateTreeCode,
+  updateBinaryCode,
+  updateLinkedListCode,
   darken,
   lighten,
   swapCode
