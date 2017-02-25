a53760bd863957a0e23ac539a7c012bac4140894
Ivey Topaz <iveytopaz@gmail.com>
2017-02-25 05:15:13 +0000

    Add actions and reducers for tracking window size

---
+++ app/actions/windowActions.js
@@ -0,0 +1,8 @@
+const updateWidth = (newWidth) => ({ type: 'UPDATE_WIDTH', newWidth });
+
+const updateHeight = (newHeight) => ({ type: 'UPDATE_HEIGHT', newHeight });
+
+export default {
+  updateWidth,
+  updateHeight
+}
---
+++ app/reducers/windowReducers.js
@@ -0,0 +1,12 @@
+const windowReducers = (state = { width: window.innerWidth, height: window.innerHeight }, action) => {
+  switch (action.type) {
+    case 'UPDATE_WIDTH':
+      return { ...state, width: action.newWidth };
+    case 'UPDATE_HEIGHT':
+      return { ...state, height: action.newHeight };
+    default:
+      return state;
+  }
+};
+
+export default windowReducers;
