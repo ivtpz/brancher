eb3de36069bf3e5677932bba8497b57716596e49
Ivey Topaz <iveytopaz@gmail.com>
2017-02-26 13:39:41 +0000

    Add linked list tutorial

---
+++ app/tutorial/listTutorial.js
@@ -0,0 +1,68 @@
+/* eslint-disable */
+const tutorialWindows = [{
+  xPos: '25%',
+  yPos: '200px',
+  text: 'You can view a visual representation of your linked list here by clicking the "Run" button'
+}, {
+  xPos: '71%',
+  yPos: '165px',
+  text: 'You have access to the functions "pause", and "highlight".'
+},  {
+  xPos: '43%',
+  yPos: '525px',
+  text: 'The pause function takes in a linked list, and puts it onto the visualizer temporarily.'
+}, {
+  xPos: '43%',
+  yPos: '525px',
+  text: 'By calling pause and passing it "newList" you can see the list in-between adding nodes.'
+}, {
+  xPos: '43%',
+  yPos: '545px',
+  text: 'Once you have passed a new list to the "pause" function, you can then call "highlight" on the nodes of that new list.'
+}, {
+  xPos: '43%',
+  yPos: '380px',
+  text: 'When traverse gets called, it passes each node to highlight in order.'
+}, {
+  xPos: '43%',
+  yPos: '380px',
+  text: 'Highlight will cause whatever node it is passed to turn red temporarily.'
+}, {
+  xPos: '35%',
+  yPos: '140px',
+  text: 'You can control the speed of animation for highlighting and viewing list states here.'
+}, {
+  xPos: '35%',
+  yPos: '140px',
+  text: 'Warning: calling highlight on a large list (25+ nodes) can run slowly.'
+}, {
+  xPos: '12%',
+  yPos: '140px',
+  text: 'You can also move backwards and forwards through your visualizations here, up to 20 steps'
+}, {
+  xPos: '45%',
+  yPos: '160px',
+  text: 'You can define properties for your list object by adding them to the "List" class.'
+}, {
+  xPos: '45%',
+  yPos: '160px',
+  text: 'The "List class needs a "head" and a "tail" property.'
+}, {
+  xPos: '45%',
+  yPos: '510px',
+  text: 'The "Node" class must have a "value" property, a "next", and an "_id" property.'
+}, {
+  xPos: '81%',
+  yPos: '505px',
+  text: 'Finally, you should return a list from this function. Whatever you return will end up in the visualizer.'
+}, {
+  xPos: '68%',
+  yPos: '300px',
+  text: 'Edit the code to create your own list...'
+}, {
+  xPos: '83%',
+  yPos: '140px',
+  text: '...then click "Run" to see it in action.'
+}]
+
+export default tutorialWindows;
